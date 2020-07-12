b708170a728c259d4598221a60648d5e113e71cc
Anika <anikakumarb@gmail.com>
2020-07-12 13:32:38 -0700

    profile form is functional and we link properly from new_profil page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
 	def main_page
+		if user_signed_in?
+			if current_user.profile
+			redirect_to new_tweet_path
+		else
+			redirect_to new_profile_path
 		@mission_statement = "Save The World"
 	end
 end
+
