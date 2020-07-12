645c17a0cffa6da5ae4c2cb88a635b7e72be73f2
Anika <anikakumarb@gmail.com>
2020-07-12 09:39:24 -0700

    added error message + character count

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
+
 	end
 
 	def create
@@ -9,9 +10,14 @@ class TweetsController < ApplicationController
 		# @tweet.save
 
 		@tweet = Tweet.new(tweet_params)
-		@tweet.save
-		flash[:success] = 'Your tweet was successfully published!'
-		redirect_to new_tweet_path
+		if @tweet.save
+		    flash[:success] = 'Your tweet was successfully published!'
+		    redirect_to new_tweet_path
+		else
+			render 'new'
+			#once we're in here, there is a .errors
+		end
+		
 	end
 
 
